f2c9c3ab5aa983ca4fc5d9473f860eae5dbbe9d8
Tobse <Tobse4Git@gmail.com>
2019-10-18 16:12:17 +0200

    🔨 Add a call of `HelloMaven.run`

--- src/main/java/de/tfr/test/UseLibTest.java
+++ src/main/java/de/tfr/test/UseLibTest.java
@@ -3,7 +3,9 @@ package de.tfr.test;
 import github.tobsef.demo.HelloMaven;
 
 public class UseLibTest {
+
     public static void main(String[] args) {
-        System.out.println(new HelloMaven().toString());
+        HelloMaven.run();
     }
+
 }
